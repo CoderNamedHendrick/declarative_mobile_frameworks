6162addc235b6236aadf0eda7d8e2ed215192400
Sebastine Odeh <sebastinesoacatp@gmail.com>
2023-09-15 22:20:40 +0100

    complete example

--- android/app/src/main/java/com/example/native_navigation/ComposeActivity.kt
+++ android/app/src/main/java/com/example/native_navigation/ComposeActivity.kt
@@ -4,19 +4,29 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -28,28 +38,15 @@ class ComposeActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AndroidTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Column (
-                        modifier = Modifier.padding(horizontal = 12.dp),
-                        verticalArrangement = Arrangement.Center
-                    ) {
-                        Greeting("Android, Welcome to Compose Side")
-                        Spacer(modifier = Modifier.height(10.dp))
-                        Button(onClick = {
-                            val replyIntent = Intent()
-                            replyIntent.putExtra(MESSAGE_REPLY, "Message from composable")
-                            setResult(RESULT_OK, replyIntent)
-                            finish()
-                        }) {
-                            Text(
-                                text = "Click me to send message to flutter side",
-                                fontSize = 14.sp,
-                            )
-                        }
+                    ComposeBody {
+                        val replyIntent = Intent()
+                        replyIntent.putExtra(MESSAGE_REPLY, it)
+                        setResult(RESULT_OK, replyIntent)
+                        finish()
                     }
                 }
             }
@@ -62,9 +59,79 @@ class ComposeActivity : ComponentActivity() {
 }
 
 @Composable
+fun ComposeBody(modifier: Modifier = Modifier, onClick: (value: String) -> Unit) {
+    var input1 by remember {
+        mutableStateOf("")
+    }
+    var input2 by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = modifier.padding(horizontal = 12.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Greeting("Enter two numbers whose sum we're going to send to flutter")
+
+        Spacer(modifier = modifier.height(15.dp))
+
+        BasicTextField(
+            value = input1,
+            modifier = modifier
+                .fillMaxWidth()
+                .background(color = MaterialTheme.colorScheme.primaryContainer)
+                .padding(6.dp),
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done,
+            ),
+            textStyle = MaterialTheme.typography.bodyLarge,
+            onValueChange = {
+                input1 = it
+            },
+        )
+        Spacer(modifier = modifier.height(10.dp))
+        BasicTextField(
+            value = input2,
+            modifier = modifier
+                .fillMaxWidth()
+                .background(color = MaterialTheme.colorScheme.primaryContainer)
+                .padding(6.dp),
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done,
+            ),
+            textStyle = MaterialTheme.typography.bodyLarge,
+            onValueChange = {
+                input2 = it
+            },
+        )
+        Spacer(modifier = modifier.height(20.dp))
+        Text(text = "Sum of inputs: ${sum(input1, input2)}")
+        Button(onClick = {
+            onClick("${sum(input1, input2)}")
+        }) {
+            Text(
+                text = "Send sum to flutter",
+                fontSize = 14.sp,
+            )
+        }
+    }
+}
+
+fun sum(input1: String, input2: String): Int {
+    val val1 = (input1.toIntOrNull() ?: 0).toInt()
+    val val2 = (input2.toIntOrNull() ?: 0).toInt()
+    return val1 + val2
+}
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "$name!",
         modifier = modifier
     )
 }
@@ -73,6 +140,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     AndroidTheme {
-        Greeting("Android")
+        ComposeBody {}
     }
 }
--- ios/Runner/Views/SwiftUIView.swift
+++ ios/Runner/Views/SwiftUIView.swift
@@ -8,28 +8,58 @@
 import SwiftUI
 
 struct SwiftUIView: View {
-    
+    @State private var input1: String = "";
+    @State private var input2: String = "";
     weak var navigationController: UINavigationController?
     weak var delegate: DelegateProtocol?
     
     var body: some View {
         VStack(alignment: .center) {
-            Text("Hello, World! from the SwiftUI side")
+            Text("Enter two numbers whose sum we're going to send to flutter")
+                .frame(alignment: .center)
+            
+            Spacer().frame(height: 12)
+            
+            TextField("Input 1", text: $input1)
+                .padding(.horizontal, 12)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .keyboardType(.numberPad)
+            
+            Spacer().frame(height: 12)
+            
+            TextField("Input 2", text: $input2)
+                .padding(.horizontal, 12)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .keyboardType(.numberPad)
             Spacer().frame(height: 20)
             
+            Text("Sum of inputs: \(calculateInputs())")
+            
             Button(action: {
-                let message: String = "Message from the swiftUI side";
-//                navigationController?.popButtonTapped(message: message)
+                let message: String = "\(calculateInputs())";
                 delegate?.popViewController(string: message)
                 navigationController?.popViewController(animated: true)
             }) {
-                Text("Click me to send message to the flutter side")
+                Text("Send sum to flutter")
             }
             .padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10))
             .background(Color.blue)
             .foregroundColor(.white)
             .cornerRadius(12)
-        }
+        }.padding(12)
+            .onTapGesture {
+                self.hideKeyboard()
+            }
+    }
+    
+    func calculateInputs() -> Int {
+        return (Int(input1) ?? 0) + (Int(input2) ?? 0)
+    }
+}
+
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
 
